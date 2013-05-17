b38567fc1a8b70e498ca428e32d1698784af7766
Michal Čihař <michal@cihar.com>
2013-05-17 14:40:52 +0200

    Sync with current code

--- demo/php/config.footer.inc.php
+++ demo/php/config.footer.inc.php
@@ -3,13 +3,13 @@
 <a href="/" target="_top">phpMyAdmin demo server</a><?php
 if (file_exists('./revision-info.php')) {
     include('./revision-info.php');
-    echo ', currently running <a href="http://wiki.phpmyadmin.net/pma/Git">Git</a> revision ';
-    echo '<a href="' . $repobase . $fullrevision . '">';
+    echo ', currently running <a target="_top" href="http://wiki.phpmyadmin.net/pma/Git">Git</a> revision ';
+    echo '<a target="_top" href="' . $repobase . $fullrevision . '">';
     echo $revision;
     echo '</a> from ';
-    echo '<a href="' . $repobranchbase . $branch . '">' . $branch . '</a> branch';
+    echo '<a target="_top" href="' . $repobranchbase . $branch . '">' . $branch . '</a> branch';
     if (!empty($reponame)) {
-        echo ' in <a href="https://github.com/' . $reponame . '/">' . $reponame . '</a> fork';
+        echo ' in <a target="_top" href="https://github.com/' . $reponame . '/">' . $reponame . '</a> fork';
     }
 } else {
     echo ', Git information missing';
