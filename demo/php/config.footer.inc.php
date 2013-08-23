cc5270f2cf90587c3439770391ef3c8e35f31843
Michal Čihař <michal@cihar.com>
2013-08-23 14:28:49 +0200

    Use GA to track demo server

--- demo/php/config.footer.inc.php
+++ demo/php/config.footer.inc.php
@@ -19,18 +19,16 @@ if (file_exists('./revision-info.php')) {
 </div>
 <div style="clear:both;"></div>
 <?php } ?>
-<!-- Piwik -->
 <script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2718724-5']);
+  _gaq.push(['_trackPageview']);
 
   (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.cihar.com/";
-    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-    _paq.push(["setSiteId", "3"]);
-    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+
 </script>
-<!-- End Piwik Code -->
