5e250291858b4bdc1790551406902fa1baafb15e
Michal Čihař <michal@cihar.com>
2014-03-13 11:28:16 +0100

    Cleanup and document SOB hook
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- hooks/sob.php
+++ hooks/sob.php
@@ -1,8 +1,13 @@
 <?php
+/**
+ * GitHub webhook to check Signed-Off-By in pull request commits.
+ */
 
 error_reporting(E_ALL);
+
 define('PMAHOOKS', True);
 
+require_once('./lib/github.php');
 
 $message = "This commit is missing Signed-Off-By line to indicate "
     . "that you agree with phpMyAdmin Developer's Certificate of Origin. "
@@ -10,20 +15,27 @@ $message = "This commit is missing Signed-Off-By line to indicate "
     . "https://github.com/phpmyadmin/phpmyadmin/blob/master/CONTRIBUTING.md"
     . ") for more information.";
 
-require_once('./lib/github.php');
-
+/* Parse JSON */
 $data = json_decode($_POST['payload'], true);
 
-if (! isset($data['pull_request'])) {
+/* Check request data */
+if (! isset($data['pull_request']) || ! isset($data['action'])) {
     die('No pull request data!');
 }
 
+/* We don't care about closed requests */
+if ($data['action'] == 'closed') {
+    die();
+}
+
+/* Parse repository name */
 $repo_name = $data['pull_request']['head']['repo']['full_name'];
 
+/* List commits in the pull request */
 $commits = github_pull_commits($data['pull_request']['number']);
 
+/* Process commits in the pull request to find one missing SOB */
 $missing = array();
-
 foreach($commits as $commit) {
     if (strpos("\nSigned-Off-By:", $commit['commit']['message']) === false) {
         github_comment_commit($repo_name, $commit['sha'], $message);
@@ -31,10 +43,7 @@ foreach($commits as $commit) {
     }
 }
 
+/* Report back that we've posted comment */
 if (count($missing) > 0) {
-//    $message = "Following commits are missing Signed-Off-By line to indicate that you agree with phpMyAdmin Developer's Certificate of Origin:\n\n";
-//    $message .= implode(', ', $missing);
-//    $message .= "\n\nPlease check [contributing documentation](https://github.com/phpmyadmin/phpmyadmin/blob/master/CONTRIBUTING.md) for more information.";
-//    github_comment($data['pull_request']['number'], $message);
     die('Comment posted.');
 }
