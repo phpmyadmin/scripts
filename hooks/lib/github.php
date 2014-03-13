673893931b7f491661b1fa2c5dc549de4f89b787
Michal Čihař <michal@cihar.com>
2014-03-13 11:01:19 +0100

    Implement SOB hook
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- hooks/lib/github.php
+++ hooks/lib/github.php
@@ -9,6 +9,14 @@ if (!defined('PMAHOOKS')) {
 
 require_once('../../.pmahook.php');
 
+$curl_base_opts = array(
+    CURLOPT_USERPWD => GITHUB_USERNAME . ':' . GITHUB_PASSWORD,
+    CURLOPT_USERAGENT => 'phpMyAdmin-bot',
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+    CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
+);
+
 /**
  * Posts a comment on GitHub pull request
  */
@@ -17,14 +25,28 @@ function github_comment($pullid, $comment)
     $ch = curl_init();
 
     //set the url, number of POST vars, POST data
-    curl_setopt($ch, CURLOPT_USERPWD, GITHUB_USERNAME . ':' . GITHUB_PASSWORD);
+    curl_setopt_array($ch, $GLOBALS['curl_base_opts']);
     curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/phpmyadmin/phpmyadmin/issues/' . $pullid . '/comments');
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'phpMyAdmin-bot');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('body' => $comment)));
+
+    //execute post
+    $result = curl_exec($ch);
+
+    //close connection
+    curl_close($ch);
+
+    return json_decode($result);
+}
+
+/**
+ * Returns list of pull request commits.
+ */
+function github_pull_commits($pullid)
+{
+    $ch = curl_init();
+    curl_setopt_array($ch, $GLOBALS['curl_base_opts']);
+    curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/phpmyadmin/phpmyadmin/pulls/' . $pullid . '/commits');
 
     //execute post
     $result = curl_exec($ch);
--- hooks/sob.php
+++ hooks/sob.php
@@ -6,6 +6,26 @@ define('PMAHOOKS', True);
 
 require_once('./lib/github.php');
 
+$data = json_decode($_POST['payload']);
 
+if (! isset($data['pull_request'])) {
+    die('No pull request data!');
+}
 
-//$ret = github_comment(774, 'PHP test comment');
+$commits = github_pull_commits($data['pull_request']['number']);
+
+$missing = array();
+
+foreach($commits as $commit) {
+    if (strpos("\nSigned-Off-By:", $commit['commit']['message']) === false) {
+        $missing[] = $commit['commit']['sha'];
+    }
+}
+
+if (count($missing) > 0) {
+    $message = "Following commits are missing Signed-Off-By:\n\n";
+    $message .= implode(', ', $missing);
+    $message .= "\n\nSee https://github.com/phpmyadmin/phpmyadmin/blob/master/CONTRIBUTING.md for more information."
+    github_comment($data['pull_request']['number'], $message);
+    die('Comment posted.');
+)
