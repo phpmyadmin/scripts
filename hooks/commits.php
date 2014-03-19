9d75cec7786f58b0ab3ad6157d9b649289fc87e4
Michal Čihař <michal@cihar.com>
2014-03-19 18:34:07 +0100

    Check for tabs in a commit as well
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- hooks/commits.php
+++ hooks/commits.php
@@ -9,11 +9,17 @@ define('PMAHOOKS', True);
 
 require_once('./lib/github.php');
 
-$message = "This commit is missing Signed-Off-By line to indicate "
+$message_sob = "This commit is missing Signed-Off-By line to indicate "
     . "that you agree with phpMyAdmin Developer's Certificate of Origin. "
     . "Please check [contributing documentation]("
     . "https://github.com/phpmyadmin/phpmyadmin/blob/master/CONTRIBUTING.md"
     . ") for more information.";
+
+$message_tab = "This commit is using tab character for indentation instead "
+    . "of spaces, what is mandated by phpMyAdmin. Please check our "
+    . "[Developer guidelines]"
+    . "(http://wiki.phpmyadmin.net/pma/Developer_guidelines#Indentation)"
+    . " for more information.";
 
 /* Parse JSON */
 $data = json_decode($_POST['payload'], true);
@@ -34,12 +40,23 @@ $repo_name = $data['pull_request']['head']['repo']['full_name'];
 /* List commits in the pull request */
 $commits = github_pull_commits($data['pull_request']['number']);
 
-/* Process commits in the pull request to find one missing SOB */
+/* Process commits in the pull request */
 foreach ($commits as $commit) {
+    /* Chek for missing SOB */
     if ( ! preg_match("@\nSigned-off-by:@i", $commit['commit']['message'])) {
-        github_comment_commit($repo_name, $commit['sha'], $message);
-        echo 'Comment on ' . $commit['sha'] . ":\n";
+        github_comment_commit($repo_name, $commit['sha'], $message_sob);
+        echo 'Comment (SOB) on ' . $commit['sha'] . ":\n";
         echo $commit['commit']['message'];
         echo "\n";
+    }
+    /* Check for tab in diff */
+    foreach ($commit['files'] as $file) {
+        if (strpos($file['patch'], "\t") !== false) {
+            github_comment_commit($repo_name, $commit['sha'], $message_tab);
+            echo 'Comment (TAB) on ' . $commit['sha'] . ":\n";
+            echo $commit['commit']['message'];
+            echo "\n";
+            break;
+        }
     }
 }
