ddfefbb5a5c438d6b7a846f07e022987160950a9
Michal Čihař <michal@cihar.com>
2014-05-10 17:05:12 +0200

    The demo server message now coming directly from our code, no need to add it separately
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- demo/php/config.footer.inc.php
+++ demo/php/config.footer.inc.php
@@ -1,24 +1,3 @@
-<?php if (substr(PMA_VERSION, 0, 3) != '4.2' && substr(PMA_VERSION, 0, 3) != '4.1') { ?>
-<script type="text/javascript" src="/common-script.js"></script>
-<div class="notice">
-<a href="/" target="_top">phpMyAdmin demo server</a><?php
-if (file_exists('./revision-info.php')) {
-    include('./revision-info.php');
-    echo ', currently running <a target="_top" href="http://wiki.phpmyadmin.net/pma/Git">Git</a> revision ';
-    echo '<a target="_top" href="' . $repobase . $fullrevision . '">';
-    echo $revision;
-    echo '</a> from ';
-    echo '<a target="_top" href="' . $repobranchbase . $branch . '">' . $branch . '</a> branch';
-    if (!empty($reponame)) {
-        echo ' in <a target="_top" href="https://github.com/' . $reponame . '/">' . $reponame . '</a> fork';
-    }
-} else {
-    echo ', Git information missing';
-}
-?>.
-</div>
-<div style="clear:both;"></div>
-<?php } ?>
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
