2650b1858145af4d5f16328b345db369a45bc56e
Michal Čihař <michal@cihar.com>
2015-12-02 10:00:51 +0100

    Disable ZeroConf on demo server without pmadb
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- demo/php/config-config-nopmadb.inc.php
+++ demo/php/config-config-nopmadb.inc.php
@@ -1,5 +1,7 @@
 <?php
 require('/home/demo/scripts/demo/php/config-common.inc.php');
+$cfg['ZeroConf'] = false;
+
 $cfg['Servers'][1]['extension'] = 'mysqli';
 $cfg['Servers'][1]['auth_type'] = 'config';
 $cfg['Servers'][2]['extension'] = 'mysqli';
