0f09bc8a97c4abb4802b594209386a0c1381b755
Michal Čihař <michal@cihar.com>
2016-09-13 17:08:26 +0200

    Improve documentation build process
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ develdocs/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Script to generate phpdoc documentation of phpMyAdmin and publish it
+# on develdocs.phpmyadmin.net
+
+# Update scripts
+cd /home/builder/scripts/
+git pull -q
+
+# Update doc build environment
+cd develdocs
+composer update
+
+# Generate docs
+for repo in phpmyadmin sql-parser motranslator shapefile ; do
+    cd /home/builder/$repo
+    git pull -q
+    if [ -d './src' ] ; then
+        SOURCE='./src'
+    else
+        SOURCE='./libraries'
+    fi
+    nice -19 /home/builder/develdocs/vendor/bin/apigen generate --source $SOURCE --destination /home/builder/scripts/output/$repo/
+done
