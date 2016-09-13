649e92023ed6468d7804735ede23ba42fd46452f
Michal Čihař <michal@cihar.com>
2016-09-13 15:57:31 +0200

    Add generic release creation hook
    Signed-off-by: Michal Čihař <michal@cihar.com>

---
+++ hooks/create_release.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * GitHub webhook to create github releases for composer packages.
+ */
+
+error_reporting(E_ALL);
+
+define('PMAHOOKS', True);
+
+require_once('./lib/github.php');
+
+github_verify_post();
+
+/* Parse JSON */
+$data = json_decode($_POST['payload'], true);
+
+if ($data['ref_type'] != 'tag') {
+    die('Not a tag');
+}
+
+$version = $tag = $data['ref'];
+
+/* Strip v prefix used on some repos */
+if (substr($version, 0, 1) == 'v') {
+    $version = substr($version, 1);
+}
+
+/* Check tag name */
+if (preg_match('/^[0-9.]+$/', $tag) === 0) {
+    die('Not a version tag!');
+}
+
+
+
+$version = implode('.', $parts);
+
+echo github_make_release(
+    $data['repository']['name'],
+    $tag,
+    $version,
+    'release ' . $version
+);
